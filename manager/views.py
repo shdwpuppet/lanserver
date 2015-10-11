471e6c3a46614d93b13cbae8b4eb62cb3dc32ccc
Alex Williams <shdw.puppet@gmail.com>
2015-10-11 15:21:58 -0400

    context bugfix

--- manager/views.py
+++ manager/views.py
@@ -67,6 +67,7 @@ def teamManager(request):
 
 @staff_member_required
 def tournamentManager(request, id=None):
+    context = {}
     if id:
         tournament = get_object_or_404(Tournament, pk=id)
         date_start = datetime.datetime.strftime(tournament.date_start, '%Y-%m-%d %H:%M')
